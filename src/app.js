97cd908cba950714d811c57ce8e08ffa17f48e6a
aniekossewaarde <116111669+aniekossewaarde@users.noreply.github.com>
2022-11-30 16:46:45 +0100

    Added relevant API icon

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
 function formatDate(timestamp) {
 let date = new Date(timestamp);
+
 let hours = date.getHours();
 if (hours < 10) {
     hours = `0${hours}`;
 }
+
 let minutes = date.getMinutes();
 if (minutes < 10) {
     minutes = `0${minutes}`;
 }
+
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
 let day = days[date.getDay()];
 return `${day} ${hours}:${minutes}`
 }
 
 let apiKey = "ab69138d433c5c85edafefc09e466c07";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Barcelona&appid=${apiKey}&units=metric`
+let city = "Barcelona";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+
 
 function displayTemperature(response) {
     console.log(response.data);
@@ -24,12 +29,15 @@ function displayTemperature(response) {
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateTimeElement = document.querySelector("#dateTime");
+    let iconElement = document.querySelector("#icon");
+
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     cityElement.innerHTML = response.data.name;
     descriptionElement.innerHTML = response.data.weather[0].description;
     humidityElement.innerHTML = response.data.main.humidity;
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateTimeElement.innerHTML = formatDate(response.data.dt * 1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 axios.get(apiUrl).then(displayTemperature);
